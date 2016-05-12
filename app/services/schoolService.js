6361ed2128c87b2b95d86e74a117db94e04c4c4d
pmanning <pxmanning@gmail.com>
2016-05-12 19:19:18 +1200

    set rest ports

--- app/services/schoolService.js
+++ app/services/schoolService.js
@@ -1,8 +1,6 @@
 var $ = require("jquery");
 var promise = require("es6-promise");
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
-var resourceUrl = "http://" + server_ip_address + ":" + server_port + "/api/schools";
+var resourceUrl = "http://" + window.location.host + "/api/schools";
 
 module.exports = {
     addSchool: function (school) {
